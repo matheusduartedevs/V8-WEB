4196ebaf762a0b7e1152f81d8e46c128f23b906f
Matheus Duarte <matheus.duartesilva42@gmail.com>
2025-02-16 13:06:08 -0300

    feat: CarCard with mock data

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import CarCard from './components/CarCard.vue';
+
+const mockCars = computed(() => [
+  { id: 1, carName: "Corolla", carBrand: "Toyota", carPrice: 120000 },
+  { id: 2, carName: "Civic", carBrand: "Honda", carPrice: 130000 },
+  { id: 3, carName: "Onix", carBrand: "Chevrolet", carPrice: 90000 },
+  { id: 4, carName: "HB20", carBrand: "Hyundai", carPrice: 85000 },
+  { id: 5, carName: "Compass", carBrand: "Jeep", carPrice: 160000 },
+  { id: 6, carName: "Kwid", carBrand: "Renault", carPrice: 70000 }
+])
+
 </script>
 
 <template>
-  <h1>V8 WEB</h1>
+  <div class="home">
+    <div v-for="cars in mockCars" :key="cars.id">
+      <CarCard :car-name="cars.carName" :car-brand="cars.carBrand" :car-price="cars.carPrice" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
+.home {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 30px;
+  padding: 10px;
+}
 </style>
---
+++ src/components/CarCard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps({
+  carName: String,
+  carBrand: String,
+  carPrice: Number
+})
+</script>
+
+<template>
+  <div class="car-card">
+    <div class="car-info">
+      <h1>{{ carName }}</h1>
+      <p>{{ carBrand }}</p>
+      <p>R$ {{ carPrice }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.car-card {
+  width: 200px;
+  border: 2px solid #FFD700;
+  border-radius: 8px;  height: 200px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  background-color: #F5F5F5;
+  color: #222222;
+}
+
+.car-info {
+  text-align: center;
+}
+</style>
