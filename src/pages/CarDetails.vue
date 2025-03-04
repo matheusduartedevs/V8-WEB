41996bc116410312792d404cbea5f37bfccf4608
Matheus Duarte <matheus.duartesilva42@gmail.com>
2025-03-04 16:10:50 -0300

    feat: implement CarDetails component with data fetching and improved styling

--- src/pages/CarDetails.vue
+++ src/pages/CarDetails.vue
@@ -1,9 +1,77 @@
 <template>
-  Car Details 
+  <div class="car">
+    <div class="car-image">
+      <img :src="car?.car_image" :alt="car?.car_name">
+    </div>
+    <div class="car-details">
+      <h1>{{ car?.car_name }}</h1>
+      <p><strong>Marca:</strong> {{ car?.car_brand }}</p>
+      <p><strong>Velocidade Máxima:</strong> {{ car?.car_top_speed }} km/h</p>
+      <p><strong>Motor:</strong> {{ car?.car_engine }}</p>
+      <p><strong>Transmissão:</strong> {{ car?.car_transmission }}</p>
+      <p><strong>Preço:</strong> R$ {{ car?.car_price }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import CarsService from '@/services/cars';
+import type { ICars } from '@/types/cars';
+import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 
+const car = ref<ICars>()
+const carsService = new CarsService()
 const route = useRoute()
+
+const fetch = async () => {
+  const id = route.params.id
+  try {
+    const data = await carsService.getCarById(id)
+    car.value = data
+  } catch (error) {
+    console.log(error)
+  } 
+}
+
+onMounted(() => {
+  fetch()
+})
+
 </script>
+<style scoped>
+.car {
+  display: flex;
+  justify-content: flex-start;
+  gap: 40px;
+  max-width: 900px;
+  height: 900px;
+  margin: 50px auto;
+  background: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+}
+
+.car-image img {
+  width: 300px;
+  height: auto;
+  border-radius: 10px;
+  object-fit: cover;
+}
+
+.car-details {
+  flex: 1; 
+  text-align: center;
+}
+
+h1 {
+  margin-bottom: 10px;
+  font-size: 24px;
+}
+
+p {
+  margin: 5px 0;
+  font-size: 16px;
+}
+</style>
--- src/services/cars.ts
+++ src/services/cars.ts
@@ -16,4 +16,20 @@ export default class CarsService {
       throw error;
     }
   }
+
+  async getCarById(id: number) {
+    try {
+      const response = await fetch(`${apiUrl}/car/${id}`);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Erro ao obter informações sobre o carro:', error);
+      throw error;
+    }
+  }
 } 
