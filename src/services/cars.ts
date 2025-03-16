b8b99d448d6b837b8614c26068754fb4ecf8a591
Matheus Duarte <matheus.duartesilva42@gmail.com>
2025-03-15 23:05:26 -0300

    feat: add mock data for cars and update CarsService to use it in development mode

--- src/services/cars.ts
+++ src/services/cars.ts
@@ -1,16 +1,23 @@
+import mockData from "@/data.json";
+
 const apiUrl = import.meta.env.VITE_CONNECTION_STRING;
+const isDevMode = import.meta.env.VITE_MODE;
 
 export default class CarsService {
   async getCars() {
     try {
-      const response = await fetch(`${apiUrl}/cars`);
-      
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+      if (isDevMode === 'development') {
+        return mockData;
+      } else {
+        const response = await fetch(`${apiUrl}/cars`);
+        
+        if (!response.ok) {
+          throw new Error(`Erro na requisição HTTP! Status: ${response.status}`);
+        }
   
-      const data = await response.json();
-      return data;
+        const data = await response.json();
+        return data;
+      }
     } catch (error) {
       console.error('Erro ao obter os carros:', error);
       throw error;
@@ -19,17 +26,21 @@ export default class CarsService {
 
   async getCarById(id: number) {
     try {
-      const response = await fetch(`${apiUrl}/car/${id}`);
-      
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
+      if (isDevMode) {
+        return mockData.find(car => car.car_id === id);
+      } else {
+        const response = await fetch(`${apiUrl}/car/${id}`);
+        
+        if (!response.ok) {
+          throw new Error(`Erro na requisição HTTP! Status: ${response.status}`);
+        }
   
-      const data = await response.json();
-      return data;
+        const data = await response.json();
+        return data;
+      }
     } catch (error) {
       console.error('Erro ao obter informações sobre o carro:', error);
       throw error;
     }
   }
-} 
+}
